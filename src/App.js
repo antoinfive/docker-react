444f6eb840d43d11f6b2004613c54eb645f4ece4
Antoin Campbell <antoin.campbell@voxmedia.com>
2021-06-25 11:33:59 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ROLLS ROYCE PULL UP, BLACK BOY HOP OUT
         </p>
         <a
           className="App-link"
